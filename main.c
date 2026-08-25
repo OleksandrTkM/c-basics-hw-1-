2c6dbcaa3988d7004d65eb659e0cb6275cfc0583
Oleksandr <TkachukOleksandrM@gmail.com>
2026-08-26 02:26:48 +0300

    Celcium to Farengeit

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+      int main(){
+          
+	     double temp1;
+             double temp2;
+
+	      printf("Enter temperature: \n");
+	      scanf("%lf", &temp1);
+
+              temp2 = temp1 * 1.8 + 32;
+
+	      printf("temperature in farengejt is: %.3lf \n", temp2);
+
+
+
+
+
+	      return 0;
+      }
